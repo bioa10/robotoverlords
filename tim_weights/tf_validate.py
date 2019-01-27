675293303f6f5f542ad31e00cf7977fd0a03ecd0
tchoate <tchoate@bondware.com>
2019-01-26 18:39:18 -0600

    Added code to save/load weights to tim_weights folder

---
+++ tim_weights/tf_validate.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+tf.keras.layers.Flatten(),
+tf.keras.layers.Dense(512, activation=tf.nn.relu),
+tf.keras.layers.Dropout(0.2),
+tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+			loss='sparse_categorical_crossentropy',
+			metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=0)
+	
+# load weights into new model
+model.load_weights("model.h5")
+print("Loaded model from disk")
+
+testing_loss = model.evaluate(x_test,y_test)
+
